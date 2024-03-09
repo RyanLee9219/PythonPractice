3352988ed50c39de7f4e8e4e7ac3dc6e6f5239d4
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 22:02:07 +0000

    lists practice1

---
+++ lists.py
@@ -0,0 +1,26 @@
+li = [1,2,3,4,5] #arrays 
+li2 = ['a','b','c']
+li3 = [1,2,'a',True] 
+
+#List slicing
+
+amazon_cart = [
+  'notebooks', 
+  'sunglasses',
+  'toys',
+  'grapes'
+]
+amazon_cart[0] = 'laptop'
+new_cart = amazon_cart[:]
+new_cart[0] = 'gum'
+print(new_cart)
+print(amazon_cart)
+
+#Matrix = two or more dimensional lists(arrays)
+matrix = [
+  [1,5,1],
+  [0,1,0],
+  [1,0,1]
+]
+
+print(matrix[0][1])
