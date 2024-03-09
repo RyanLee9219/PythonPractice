fe1223f03c19134c1c39f82fa0c0045621ed65ae
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 21:04:38 +0000

    type conversion practice

---
+++ type_conversion.py
@@ -0,0 +1,10 @@
+# name = 'ryan'
+# age =32
+# relationship_status = 'married'
+
+# relationship_status = 'it\'s complicated'
+
+birth_year = input('what year were you born?: ')
+
+age = 2024 - int(birth_year)
+print(f'your age is: {age}')
