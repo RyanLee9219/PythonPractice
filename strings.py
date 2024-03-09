105a29d43640db9437bff258fa3c54dee34c2520
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 17:53:58 +0000

    python3 learning string

---
+++ strings.py
@@ -0,0 +1,26 @@
+print(type("hi hello there 24!"))
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WOW
+O O
+___
+'''
+print(long_string)
+first_name = 'Ryan'
+last_name = 'lee'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+#string concatenation 
+print('hello' + ' Ryan')
+
+#Type conversion
+print(type(str(100))) #string 
+print(type(int(str(100)))) #int
+
+a = str(100)
+b = int(a)
+c = type(b)
+print(c)
+
