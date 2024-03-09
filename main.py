87b772185511e83c8b01987c59d39fb69d99d7c3
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 16:17:05 +0000

    python3 numbers practice

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print ('hello World');
+# #Fundamental data types
+# print(type(6))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4)) #0.5
+
+# #power
+# print(2 ** 3)
+# print(5 // 4) #rounded down into integer
+# print(5 % 4)
+
+#math functions 
+print(round(3.9))
+print(abs(-15))
+
+#operator precedence 
+print((20-3) + 2 ** 2)
+#()
+#**
+#  * /
+#  + -
+print((5+4) * 10 / 2) #45
+print(((5+4) * 10) / 2) #45
+print((5+4) * (10 / 2)) #45
+print(5 + (4 * 10) / 2) #25
+print(5 + 4 * 10 // 2) #25  
