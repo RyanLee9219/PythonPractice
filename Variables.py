d7181b8e18d3881a035a3119c823a4fac8483014
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 17:31:38 +0000

    python3 Variables learning

---
+++ Variables.py
@@ -0,0 +1,19 @@
+user_IQ = 190
+print(user_IQ)
+user_age = user_IQ / 4
+a =user_age
+print(a)
+
+#constants
+PI = 3.14
+PI = 0
+print(PI)
+
+#dunder
+#__
+
+#multiple declarations
+a,b,c =1,2,3
+print(a)
+print(b)
+print(c)
