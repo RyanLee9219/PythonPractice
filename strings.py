d87b4e6e40759e789afe4c89d489d871f3bc8f9d
Ryan Lee <144248490+RyanLee9219@users.noreply.github.com>
2024-03-09 20:07:00 +0000

    more practice concept of String

--- strings.py
+++ strings.py
@@ -24,3 +24,35 @@ b = int(a)
 c = type(b)
 print(c)
 
+#formatted strings
+
+name = 'Ryan'
+age = 31
+
+# print('hi ' + name + '. You are ' + str(age) + ' years old')
+
+# print(f'hi {name}. You are {age} years old') **
+
+# print('hi {}. You are {} years old'.format('Ryan', '31'))
+# print('hi {}. You are {} years old'.format(name, age))
+
+# print('hi {1}. You are {0} years old'.format(name, age))
+
+print('hi {new_name}. You are {age} years old'.format(new_name='sally',age =100))
+
+selfish = 'me me me'
+        #  01234567  
+print(selfish [0])
+selfish = '01234567'
+print(selfish[0:8])
+print(selfish[0:8:2])#0246
+print(selfish[1:]) #1234567
+print(selfish[:5]) #01234
+print(selfish[::1]) #default behavior
+print(selfish[-1]) #7
+print(selfish[::-1]) #76543210 reverse order of String **
+#selfish[start:stop:stepover]
+#immutability
+selfish = '01234567'
+selfish = selfish + '8'
+print(selfish)
